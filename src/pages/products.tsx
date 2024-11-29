80b8d95bac3b0e578fe37e2bfbf2a062a8dc5c4e
AsjadAkber <ma3928@drexel.edu>
2024-11-29 08:09:19 -0500

    Added products page

---
+++ src/pages/products.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ProductsPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-bold text-gray-900">Products</h1>
+        <p className="mt-4 text-gray-600">
+          Explore our curated selection of handmade products from Kashmir artisans.
+        </p>
+        {/* Add your products list here */}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
+
